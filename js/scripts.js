6b87e5105ce54d73c11c41c81f7e583f4f1b73cf
Liam Stabeno <liamstabeno@gmail.com>
2017-02-17 10:19:17 -0800

    create initial index file, js files, and css files.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+$(function(){
+  $("form#formInfo").submit(function(){
+    event.preventDefault();
+    var name = $("#name").val();
+    $(".name").text(name);
+    
+
+  });
+
+var php = 0;
+var java = 0;
+var ruby = 0;
+var c = 0;
+
+var respond = function(q1, q2, q3, q4, q5, q6, q7)  {
+}
+});
